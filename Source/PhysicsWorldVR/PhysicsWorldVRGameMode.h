53f7321741115d34ed8e177cf06f96f14612a893
Alex Babich <apb2045@gmail.com>
2015-06-03 15:26:00 -0400

    first commit

---
+++ Source/PhysicsWorldVR/PhysicsWorldVRGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "PhysicsWorldVRGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PHYSICSWORLDVR_API APhysicsWorldVRGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
